085d046d1e3e13d83379e005b9c017a6a546845a
Илья Вожжов <voggov1@mail.ru>
2022-12-20 08:31:23 +0300

    lab6

---
+++ Lab6/Lab6/Lab6.cpp
@@ -0,0 +1,55 @@
+﻿// Lab6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <conio.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream> 
+using namespace std;
+typedef BOOL(*function)();
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	HINSTANCE hLib = LoadLibraryA("../Debug/dll.dll");
+	if (hLib == NULL)
+		printf("Error loading library\n");
+	else
+	{
+		function SetHook = (function)GetProcAddress(hLib, "?SetHook@@YAHXZ");
+		function DeleteHook = (function)GetProcAddress(hLib, "?DeleteHook@@YAHXZ");
+		if (SetHook == NULL || DeleteHook == NULL)
+		{
+			printf("Error dll import.\n");
+			FreeLibrary(hLib);
+		}
+		else
+		{
+			if (SetHook())
+			{
+				printf("Proc hooked.\nPress any Key to unhook.\n");
+				_getch();
+				if (DeleteHook())
+					printf("Unhooked.\n");
+				else
+					printf("Error unsetting.\n");
+			}
+			else
+				printf("Error while setting hook.\n");
+		}
+	}
+	printf("Press any key to exit program.\n");
+	_getch();
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lab6/dll/dllmain.cpp
@@ -0,0 +1,61 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+
+HHOOK hHook;
+HMODULE hDll;
+
+LRESULT CALLBACK HookFunction(int Code, WPARAM wParam, LPARAM lParam) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	if (Code >= 0) {
+		if (Code == HCBT_DESTROYWND)
+		{
+			wchar_t winName[512];
+			GetWindowText((HWND)wParam, LPWSTR(winName), 512);
+			MessageBox(NULL, L"Пользователь закрыл окно!", L"Уведомление!", NULL);
+		}
+	}
+	else {
+		return CallNextHookEx(hHook, Code, wParam, lParam);
+	}
+	CallNextHookEx(hHook, Code, wParam, lParam);
+	return 0;
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		hDll = hModule;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+
+}
+
+__declspec(dllexport) BOOL SetHook() {
+	if (hHook = SetWindowsHookEx(WH_CBT, HookFunction, hDll, 0))
+		return true;
+	else
+		return false;
+}
+
+__declspec(dllexport) BOOL DeleteHook() {
+	if (UnhookWindowsHookEx(hHook))
+		return true;
+	else
+		return false;
+}
+
+
+
